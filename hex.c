c8b87a3afea60b23b300de66d6f619d76eec1daf
Biohazard <biohazard@somewhere>
2013-05-15 17:41:47 +1000

    astyle detached dereference operators as multiply

--- hex.c
+++ hex.c
@@ -20,18 +20,17 @@
 
 ssize_t HexEncode(const char* src, size_t srcLen, char* dst, size_t dstSize)
 {
-    static const char lookup[] = { '0', '1', '2', '3', '4',
-                                   '5', '6', '7', '8', '9',
-                                   'A', 'B', 'C', 'D', 'E',
-                                   'F'
-                                 };
+    static const char lookup[] = {  '0', '1', '2', '3', '4',
+                                    '5', '6', '7', '8', '9',
+                                    'A', 'B', 'C', 'D', 'E', 
+                                    'F' };
 
     char* dp = dst;
     const char* sp;
     ssize_t dstLen = 0;
     for (sp = src; srcLen > 0 && dstLen + 2 < (ssize_t) dstSize; sp++) {
 
-        unsigned int ascii = (unsigned int) * sp;
+        unsigned int ascii = (unsigned int) *sp;
         unsigned int digit2 = ascii % 16;
         ascii /= 16;
         unsigned int digit1 = ascii % 16;
@@ -76,10 +75,10 @@ ssize_t HexDecode(const char* src, size_t srcLen, char* dst, size_t dstSize)
     ssize_t dstLen = 0;
     for (sp = src; srcLen > 0 && dstLen + 1 < (ssize_t) dstSize; sp += 2) {
 
-        int digit1 = HexDecodeChar((unsigned int) * sp);
+        int digit1 = HexDecodeChar((unsigned int) *sp);
         if (digit1 == -1) { return -1; }
 
-        int digit2 = HexDecodeChar((unsigned int) * (sp + 1));
+        int digit2 = HexDecodeChar((unsigned int) *(sp + 1));
         if (digit2 == -1) { return -1; }
 
         *dp++ = digit1 * 16 + digit2;
